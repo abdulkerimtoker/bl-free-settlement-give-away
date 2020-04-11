0701bcefbeb053e8e8f4ee732e3e6d249aecfd5d
Abdulkerim Toker <tokerabdulkerim@gmail.com>
2020-04-11 19:45:13 +0300

    Init

---
+++ FreeSettlementGiveAway/KingdomAnexation.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.ViewModelCollection.KingdomManagement.KingdomSettlement;
+using TaleWorlds.Core;
+
+namespace FreeSettlementGiveAway
+{
+    [HarmonyPatch(typeof(KingdomSettlementVM))]
+    [HarmonyPatch("ExecuteAnnex")]
+    public class KingdomAnexationPatch
+    {
+        static bool Prefix(KingdomSettlementItemVM ____currentSelectedSettlement)
+        {
+            if (____currentSelectedSettlement.Settlement.OwnerClan == Clan.PlayerClan)
+            {
+                var name = ____currentSelectedSettlement.Settlement.Name;
+                InformationManager.DisplayMessage(new InformationMessage($"You have given away {name}"));
+                ChangeOwnerOfSettlementAction.ApplyBySiege(Clan.PlayerClan.MapFaction.Leader, 
+                    null, ____currentSelectedSettlement.Settlement);
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(KingdomSettlementVM))]
+    [HarmonyPatch("AnnexCost", MethodType.Setter)]
+    public class KingdomAnexationCostPatch
+    {
+        static void Prefix(KingdomSettlementItemVM ____currentSelectedSettlement, ref int value)
+        {
+            if (____currentSelectedSettlement != null &&
+                ____currentSelectedSettlement.Settlement.OwnerClan == Clan.PlayerClan)
+            {
+                value = 0;
+            }
+        }
+    }
+}
---
+++ FreeSettlementGiveAway/Main.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace FreeSettlementGiveAway
+{
+    public class Main : MBSubModuleBase
+    {
+        private static bool _isPatched = false;
+
+        protected override void OnSubModuleLoad()
+        {
+            PatchAll();
+        }
+
+        private void PatchAll()
+        {
+            if (_isPatched) 
+                return;
+            
+            var harmony = new Harmony("FreeSettlementGiveAway");
+            harmony.PatchAll();
+            _isPatched = true;
+        }
+    }
+}
